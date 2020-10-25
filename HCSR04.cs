287dc7e8138da45868e580c1fb24f0fad2fb9ec3
MihneaSeuleanu23 <68143614+MihneaSeuleanu23@users.noreply.github.com>
2020-10-25 18:37:52 +0200

    Mishu

---
+++ HCSR04.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace Workshop_IOT
+{
+    public class HCSR04
+    {
+        GpioController gpio = GpioController.GetDefault();
+
+        GpioPin TriggerPin;
+        GpioPin EchoPin;
+
+        public HCSR04(int TriggerPin, int EchoPin)
+        {
+            this.TriggerPin = gpio.OpenPin(TriggerPin);
+            this.EchoPin = gpio.OpenPin(EchoPin);
+
+            this.TriggerPin.SetDriveMode(GpioPinDriveMode.Output);
+            this.EchoPin.SetDriveMode(GpioPinDriveMode.Input);
+
+            this.TriggerPin.Write(GpioPinValue.Low);
+        }
+
+        public double GetDistance()
+        {
+            ManualResetEvent mre = new ManualResetEvent(false);
+            mre.WaitOne(500);
+            Stopwatch pulseLength = new Stopwatch();
+
+            //Send pulse
+            TriggerPin.Write(GpioPinValue.High);
+            mre.WaitOne(TimeSpan.FromMilliseconds(0.01));
+            TriggerPin.Write(GpioPinValue.Low);
+
+            while (EchoPin.Read() == GpioPinValue.Low) { }
+            pulseLength.Start();
+
+            while (EchoPin.Read() == GpioPinValue.High) { }
+            pulseLength.Stop();
+
+            TimeSpan timeBetween = pulseLength.Elapsed;
+            Debug.WriteLine(timeBetween.ToString());
+            double distance = timeBetween.TotalSeconds * 17000;
+
+            return distance;
+        }
+    }
+
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Workshop_IOT
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        HCSR04 hCSR04;
+
+        public MainPage()
+        {
+            hCSR04 = new HCSR04(18, 24);
+            InitializeComponent();
+        }
+
+        private void MeasureButton_Click(object sender, RoutedEventArgs e)
+        {
+            double distance=hCSR04.GetDistance();
+            tb.Text = distance.ToString();
+        }
+    }
+}
